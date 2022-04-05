88b5d761ec4f815b89edd75e18b93e8b4531b71b
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2022-04-05 14:27:42 +0200

    Change blog style

--- resources/static/js/data/categoryInfo.js
+++ resources/static/js/data/categoryInfo.js
@@ -12,119 +12,148 @@ window.categoryInfo = {
             "subCategory": [
                 {
                     "title": "Personal Finance 1: What is Money?",
-                    "url": "/what-is-money/"
+                    "url": "/what-is-money/",
+                    "imageUrl": "/img/personal-finance/what-is-money.jpg"
                 },
                 {
                     "title": "Personal Finance 2: Where should we Begin?",
-                    "url": "/where-should-we-begin/"
+                    "url": "/where-should-we-begin/",
+                    "imageUrl": "/img/personal-finance/where-should-we-begin.jpg"
                 },
                 {
                     "title": "Personal Finance 3: Why the Rich get Richer?",
-                    "url": "/why-the-rich-get-richer/"
+                    "url": "/why-the-rich-get-richer/",
+                    "imageUrl": "/img/personal-finance/why-the-rich-get-richer.jpg"
                 },
                 {
                     "title": "Personal Finance 4: Are you Rich if you own a BMW?",
-                    "url": "/are-you-rich-if-you-own-a-bmw/"
+                    "url": "/are-you-rich-if-you-own-a-bmw/",
+                    "imageUrl": "/img/personal-finance/are-you-rich-if-you-own-a-bmw.jpg"
                 },
                 {
                     "title": "Personal Finance 5: Is your Home an Asset or a Liability?",
-                    "url": "/is-your-home-an-asset-or-liability/"
+                    "url": "/is-your-home-an-asset-or-liability/",
+                    "imageUrl": "/img/personal-finance/is-your-home-an-asset-or-liability.jpg"
                 },
                 {
                     "title": "Personal Finance 6: Self Analysis! Understanding your habits?",
-                    "url": "/self-analysis-understanding-your-habits/"
+                    "url": "/self-analysis-understanding-your-habits/",
+                    "imageUrl": "/img/personal-finance/self-analysis-understanding-your-habits.jpg"
                 },
                 {
                     "title": "Personal Finance 7: Self Motivation! A necessity for success?",
-                    "url": "/self-motivation-a-necessity-for-success/"
+                    "url": "/self-motivation-a-necessity-for-success/",
+                    "imageUrl": "/img/personal-finance/self-motivation-a-necessity-for-success.jpg"
                 },
                 {
                     "title": "Personal Finance 8: Find your desire for Financial Freedom",
-                    "url": "/find-your-desire-for-financial-freedom/"
+                    "url": "/find-your-desire-for-financial-freedom/",
+                    "imageUrl": "/img/personal-finance/find-your-desire-for-financial-freedom.jpg"
                 },
                 {
                     "title": "Personal Finance 9: Find what Drives you to Achieve",
-                    "url": "/find-what-drives-you-to-achieve/"
+                    "url": "/find-what-drives-you-to-achieve/",
+                    "imageUrl": "/img/personal-finance/find-what-drives-you-to-achieve.jpg"
                 },
                 {
                     "title": "Personal Finance 10: Include your Partner",
-                    "url": "/include-your-partner/"
+                    "url": "/include-your-partner/",
+                    "imageUrl": "/img/personal-finance/include-your-partner.jpg"
                 },
                 {
                     "title": "Personal Finance 11: The Power of Dreaming",
-                    "url": "/power-of-dreaming/"
+                    "url": "/power-of-dreaming/",
+                    "imageUrl": "/img/personal-finance/power-of-dreaming.jpg"
                 },
                 {
                     "title": "Personal Finance 12: Do you really need a budget to become Financially Free?",
-                    "url": "/do-you-really-need-a-budget-to-become-financially-free/"
+                    "url": "/do-you-really-need-a-budget-to-become-financially-free/",
+                    "imageUrl": "/img/personal-finance/do-you-really-need-a-budget-to-become-financially-free.jpg"
                 },
                 {
                     "title": "Personal Finance 13: I cannot save a Penny More",
-                    "url": "/i-cannot-save-a-penny-more/"
+                    "url": "/i-cannot-save-a-penny-more/",
+                    "imageUrl": "/img/personal-finance/i-cannot-save-a-penny-more.jpg"
                 },
                 {
                     "title": "Personal Finance 14: Income Allocation Strategy",
-                    "url": "/income-allocation-strategy/"
+                    "url": "/income-allocation-strategy/",
+                    "imageUrl": "/img/personal-finance/income-allocation-strategy.jpg"
                 },
                 {
                     "title": "Personal Finance 15: Emergency Fund: Why is it important?",
-                    "url": "/emergency-fund-why-is-it-important/"
+                    "url": "/emergency-fund-why-is-it-important/",
+                    "imageUrl": "/img/personal-finance/emergency-fund-why-is-it-important.jpg"
                 },
                 {
                     "title": "Personal Finance 16: Debt Analysis: Analysing Your Financial Past?",
-                    "url": "/debt-analysis-analysing-your-financial-past/"
+                    "url": "/debt-analysis-analysing-your-financial-past/",
+                    "imageUrl": "/img/personal-finance/debt-analysis-analysing-your-financial-past.jpg"
                 },
                 {
                     "title": "Personal Finance 17: Debt Management Strategy",
-                    "url": "/debt-management-strategy/"
+                    "url": "/debt-management-strategy/",
+                    "imageUrl": "/img/personal-finance/debt-management-strategy.jpg"
                 },
                 {
                     "title": "Personal Finance 18: Credit Cards: How to Increase your Credit Score",
-                    "url": "/credit-cards-how-to-increase-your-credit-score/"
+                    "url": "/credit-cards-how-to-increase-your-credit-score/",
+                    "imageUrl": "/img/personal-finance/credit-cards-how-to-increase-your-credit-score.jpg"
                 },
                 {
                     "title": "Personal Finance 19: Home Loan: Is it a Boon in Disguise",
-                    "url": "/home-loan-is-it-a-boon-in-disguise/"
+                    "url": "/home-loan-is-it-a-boon-in-disguise/",
+                    "imageUrl": "/img/personal-finance/home-loan-is-it-a-boon-in-disguise.jpg"
                 },
                 {
                     "title": "Personal Finance 20: Savings Management Strategy",
-                    "url": "/savings-management-strategy/"
+                    "url": "/savings-management-strategy/",
+                    "imageUrl": "/img/personal-finance/savings-management-strategy.jpg"
                 },
                 {
                     "title": "Personal Finance 21: Mark Zuckerberg Didn’t Save His Way To Financial Freedom",
-                    "url": "/mark-zuckerberg-didnt-save-his-way-to-financial-freedom/"
+                    "url": "/mark-zuckerberg-didnt-save-his-way-to-financial-freedom/",
+                    "imageUrl": "/img/personal-finance/mark-zuckerberg-didnt-save-his-way-to-financial-freedom.jpg"
                 },
                 {
                     "title": "Personal Finance 22: How to Save Your Way to Achieve Your Dream",
-                    "url": "/how-to-save-your-way-to-achieve-your-dream/"
+                    "url": "/how-to-save-your-way-to-achieve-your-dream/",
+                    "imageUrl": "/img/personal-finance/how-to-save-your-way-to-achieve-your-dream.jpg"
                 },
                 {
                     "title": "Personal Finance 23: Buying a Car the Right Way",
-                    "url": "/buying-a-car-the-right-way/"
+                    "url": "/buying-a-car-the-right-way/",
+                    "imageUrl": "/img/personal-finance/buying-a-car-the-right-way.jpg"
                 },
                 {
                     "title": "Personl Finance 24: Never Kill Your Golden Goose",
-                    "url": "/never-kill-your-golden-goose/"
+                    "url": "/never-kill-your-golden-goose/",
+                    "imageUrl": "/img/personal-finance/never-kill-your-golden-goose.jpg"
                 },
                 {
                     "title": "Personl Finance 25: Financial Goals to Achieve Before You Turn 35",
-                    "url": "/financial-goals-to-achieve-before-you-turn-thirty-five/"
+                    "url": "/financial-goals-to-achieve-before-you-turn-thirty-five/",
+                    "imageUrl": "/img/personal-finance/financial-goals-to-achieve-before-you-turn-thirty-five.jpg"
                 },
                 {
                     "title": "Personl Finance 26: Investment Management Strategy",
-                    "url": "/investment-management-strategy/"
+                    "url": "/investment-management-strategy/",
+                    "imageUrl": "/img/personal-finance/investment-management-strategy.jpg"
                 },
                 {
                     "title": "Personl Finance 27: Investing in Mutual Funds",
-                    "url": "/investing-in-mutual-funds/"
+                    "url": "/investing-in-mutual-funds/",
+                    "imageUrl": "/img/personal-finance/investing-in-mutual-funds.jpg"
                 },
                 {
                     "title": "Personl Finance 28: Investing in Index Funds",
-                    "url": "/investing-in-index-funds/"
+                    "url": "/investing-in-index-funds/",
+                    "imageUrl": "/img/personal-finance/investing-in-index-funds.jpg"
                 },
                 {
                     "title": "Personl Finance 29: Investing in Fixed Deposits",
-                    "url": "/investing-in-fixed-deposits/"
+                    "url": "/investing-in-fixed-deposits/",
+                    "imageUrl": "/img/personal-finance/investing-in-fixed-deposits.jpg"
                 }
             ]
         },
@@ -135,54 +164,49 @@ window.categoryInfo = {
             "subCategory": [
                 {
                     "title": "The Truth about White Lies",
-                    "url": "/the-truth-about-white-lies/"
+                    "url": "/the-truth-about-white-lies/",
+                    "imageUrl": "/img/psychology/the-truth-about-white-lies.jpg"
                 },
                 {
                     "title": "What is your Religion?",
-                    "url": "/what-is-your-religion/"
+                    "url": "/what-is-your-religion/",
+                    "imageUrl": "/img/psychology/what-is-your-religion.jpg"
                 },
                 {
                     "title": "Why is the Sense of Morality more Important than Capitalism",
-                    "url": "/why-is-the-sense-of-morality-more-important-than-capitalism/"
+                    "url": "/why-is-the-sense-of-morality-more-important-than-capitalism/",
+                    "imageUrl": "/img/psychology/why-is-the-sense-of-morality-more-important-than-capitalism.jpg"
                 },
                 {
                     "title": "Is Racism a Social Construct?",
-                    "url": "/is-racism-a-social-construct/"
+                    "url": "/is-racism-a-social-construct/",
+                    "imageUrl": "/img/psychology/is-racism-a-social-construct.jpg"
                 },
                 {
                     "title": "Pursuit of Problem Solving",
-                    "url": "/pursuit-of-problem-solving/"
+                    "url": "/pursuit-of-problem-solving/",
+                    "imageUrl": "/img/psychology/pursuit-of-problem-solving.jpg"
                 },
                 {
                     "title": "Can a Smoke ask you to Not Smoke?",
-                    "url": "/can-a-smoker-ask-you-to-not-smoke/"
-                }
-            ]
-        },
-        {
-            "categoryName": "One Piece",
-            "dataUrl": "/psychology/one-piece/",
-            "description": "One piece has a lot to offer!",
-            "subCategory": [
+                    "url": "/can-a-smoker-ask-you-to-not-smoke/",
+                    "imageUrl": "/img/psychology/can-a-smoker-ask-you-to-not-smoke.jpg"
+                },
+                {
+                    "title": "Search for the Good: Self preservation",
+                    "url": "/search-for-the-good/self-preservation/",
+                    "imageUrl": "/img/psychology/search-for-the-good/self-preservation.jpg"
+                },
                 {
                     "title": "One Piece: Straw Hat Luffy",
-                    "url": "/straw-hat-luffy/"
+                    "url": "/one-piece/straw-hat-luffy/",
+                    "imageUrl": "/img/psychology/one-piece/straw-hat-luffy.jpg"
                 },
                 {
                     "title": "One Piece: Roronova Zoro",
-                    "url": "/roronoa-zoro/"
+                    "url": "/one-piece/roronoa-zoro/",
+                    "imageUrl": "/img/psychology/one-piece/roronoa-zoro.jpg"
                 }
-            ]
-        },
-        {
-            "categoryName": "Search for the Good",
-            "dataUrl": "/psychology/search-for-the-good/",
-            "description": "Search for the goodness in ourselves",
-            "subCategory": [
-                {
-                    "title": "Search for the Good: Self preservation",
-                    "url": "/self-preservation/"
-                },
             ]
         }
     ]
---
+++ resources/static/js/pages/home/blogList.js
@@ -0,0 +1,38 @@
+function populateAllBlogs(categoryInfo) {
+    let documentFragment = document.createDocumentFragment();
+
+    for (let i = 0, len = categoryInfo.length; i < len; i++) {
+        let category = categoryInfo[i];
+        let categoryrUrl = category.dataUrl;
+        let subCategoryNav = category.subCategory;
+
+        if (isEmpty(subCategoryNav)) {
+            return subCategoryDiv;
+        }
+
+        for (let j = 0, length = subCategoryNav.length; j < length; j++) {
+            let subCategoryNavItem = subCategoryNav[j];
+            let column = document.createElement('div');
+            column.classList = 'col-12 col-md-6 col-lg-6 text-animation fIU';
+
+            let anchor = document.createElement('a');
+            anchor.href = '/' + window.currentLanguage + categoryrUrl.slice(0, -1) + subCategoryNavItem.url;
+            anchor.classList = 'category mb-5';
+
+            let image = document.createElement('img');
+            image.src = subCategoryNavItem.imageUrl;
+            image.classList = 'w-100';
+            anchor.appendChild(image);
+
+            let heading = document.createElement('h4');
+            heading.classList = 'headerResult';
+            heading.textContent = subCategoryNavItem.title;
+            anchor.appendChild(heading);
+
+            column.appendChild(anchor);
+            documentFragment.appendChild(column);
+        }
+    }
+
+    return documentFragment;
+}
